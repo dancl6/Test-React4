8d5139abad8ce22b5de5a3cd8e50d1a56525e978
Dan Lyons <danielclyons@yahoo.com>
2020-08-12 00:10:14 -0400

    build

---
+++ build/precache-manifest.6bdf46ec1994b4a95fe991b7bb7c5c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f025a95a90f74297a78ead1c5a74adbc",
+    "url": "/Test-React4/index.html"
+  },
+  {
+    "revision": "56273efb156e5fddef39",
+    "url": "/Test-React4/static/css/main.44f96915.chunk.css"
+  },
+  {
+    "revision": "0992120817076dd5b8a5",
+    "url": "/Test-React4/static/js/2.28c68744.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Test-React4/static/js/2.28c68744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56273efb156e5fddef39",
+    "url": "/Test-React4/static/js/main.a33d59d7.chunk.js"
+  },
+  {
+    "revision": "d43bb53b726009474528",
+    "url": "/Test-React4/static/js/runtime-main.58ffc647.js"
+  }
+]);
