a3fc3e432ddc056affe5ad5ab89d01252a4f91d7
Dan Lyons <danielclyons@yahoo.com>
2020-08-25 17:07:41 -0400

    update with workout tracker

---
+++ build/precache-manifest.a97535d84a9a9f44e887a2f573b22aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec7160425bfd720bddd3d6764bb5a79",
+    "url": "/Test-React4/index.html"
+  },
+  {
+    "revision": "0f9aaa83414d98828533",
+    "url": "/Test-React4/static/css/main.44f96915.chunk.css"
+  },
+  {
+    "revision": "0992120817076dd5b8a5",
+    "url": "/Test-React4/static/js/2.28c68744.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Test-React4/static/js/2.28c68744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9aaa83414d98828533",
+    "url": "/Test-React4/static/js/main.0b6b0cb2.chunk.js"
+  },
+  {
+    "revision": "d43bb53b726009474528",
+    "url": "/Test-React4/static/js/runtime-main.58ffc647.js"
+  }
+]);
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
-import '../../index.css';
+// import '../../index.css';
+
 // import image1 from '../../../public/PasswordGenerator.png'
 // export default {
 //     image1
@@ -45,14 +49,22 @@ function Project() {
             deployed: 'https://arcane-anchorage-18356.herokuapp.com/',
             image: 'TechStopNShop.png',
             name: 'Tech Stop-N-Shop'
+        },
+        {
+            github: 'https://github.com/aelisker/workout-tracker',
+            deployed: 'https://adj-workout-tracker.herokuapp.com/',
+            image: 'Workout-Tracker.png',
+            name: 'Workout Tracker'
         }
     ])
     console.log("i am at projects console log")
     console.log(projects[0].image)
     return (
     <div>
+    <Container  key="Container" >
+    <Row  key= "Row" >
         {projects.map((project,i) =>(
-
+    <Col sm={12} md={6} key={i}>
     <Card  key={project.name} className="bg-dark text-white card-background set-padding">
     <Card.Img key={project.image} src= {process.env.PUBLIC_URL+"/"+project.image} alt="Card image" className = "image-size"/>
     
@@ -67,8 +79,10 @@ function Project() {
         <Card.Link key={project.deployed} href={project.deployed}>{project.name}</Card.Link>
     </Card.ImgOverlay>
     </Card>
-
+    </Col>
         ))}
+    </Row>
+    </Container>
     </div>
     )
 }
