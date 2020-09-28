4b279bade9c515a62afa44aea8d18fd49f63806f
Dan Lyons <danielclyons@yahoo.com>
2020-09-27 22:46:58 -0400

    contact info and image

---
+++ build/precache-manifest.c2df9fd1080bb77c974a271a8d052584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd835793f5bdc4a24e8aa9ab48c61a84",
+    "url": "/Test-React4/index.html"
+  },
+  {
+    "revision": "df005b1e0def61d03732",
+    "url": "/Test-React4/static/css/main.d1454c89.chunk.css"
+  },
+  {
+    "revision": "f6f4051bcb0a09d17603",
+    "url": "/Test-React4/static/js/2.5ef35653.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Test-React4/static/js/2.5ef35653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df005b1e0def61d03732",
+    "url": "/Test-React4/static/js/main.134f6dfe.chunk.js"
+  },
+  {
+    "revision": "d43bb53b726009474528",
+    "url": "/Test-React4/static/js/runtime-main.58ffc647.js"
+  }
+]);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ function Header(props) {
     return (
         <header className="flex-row px-1 header-stylin">
         <h2>
-          <a data-testid="link" href="/">
+          <a data-testid="link" href="/Test-React4">
             <span role="img" > </span> Dan Lyons Portfolio
           </a>
         </h2>
