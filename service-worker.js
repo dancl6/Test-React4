7c2673df5175224f57d9e640a3fdf73caaee84be
Dan Lyons <danielclyons@yahoo.com>
2020-09-27 22:05:44 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Test-React4/precache-manifest.7246547886ae33a393ae10ea049214aa.js"
+  "/Test-React4/precache-manifest.c2df9fd1080bb77c974a271a8d052584.js"
 );
 
 self.addEventListener('message', (event) => {
